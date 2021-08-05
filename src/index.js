208562da026d2315bb66f4f8e28bb457cd7857f5
hunglethe2000 <hungvirut2000@gmail.com>
2021-08-05 12:44:07 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const person = {
-  name: 'le the hung',
-  age: 21,
-  status: true
-}
-function total(giatriA, giatriB){
-  return giatriA + giatriB;
-}
-const a = 20;
-const b = 10;
-const element = <h1>Sum = {total(a,b)}</h1>
-ReactDOM.render(
-    element,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
